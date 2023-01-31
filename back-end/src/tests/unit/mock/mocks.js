0ca49f2b84c334f3597757ce4cec1af1ec0bc166
IsmaelDantas <ismcondan2019@gmail.com>
2023-01-31 15:55:52 -0300

    [MAIN GROUP 15] [TESTES BACKEND] [Feat: Criação de mocks e pastas para controller e service de products]

---
+++ back-end/src/tests/unit/mock/mocks.js
@@ -0,0 +1,22 @@
+const productMocks = [
+    {
+        id: 1,
+        name: 'Mista Campari Bitter',
+        price: 44.90,
+        urlImage: 'https://superprix.vteximg.com.br/arquivos/ids/169298-600-600/Bebida-Alcoolica-Mista-Campari-Bitter-900ml.jpg?v=636094502129230000'
+    },
+    {
+        id: 2,
+        name: 'Vodka Nordka 900ml',
+        price: 28.99,
+        urlImage: 'https://bomdebeer.vtexassets.com/arquivos/ids/157377/Vodka-Nordka-900ml-7897395001322_2.png?v=637883297367470000'
+    },
+    {
+        id: 3,
+        name: 'Heineken Garrafa 330ml',
+        price: 5.98,
+        urlImage: 'https://giassi.vtexassets.com/arquivos/ids/541297/Cerveja-Lager-Heineken-Garrafa-330ml.png?v=638053322320370000'
+    }
+]
+
+module.exports = productMocks;
