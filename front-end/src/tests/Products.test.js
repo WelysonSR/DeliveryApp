3662137a697af3889b9e2e203699f72821f59769
paulo <paulo_alexandre_slipknot@hotmail.com>
2023-02-06 07:12:20 -0400

    test: adiciona os testes da tela de produtos

--- front-end/src/tests/Products.test.js
+++ front-end/src/tests/Products.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import renderWithRouter from './RenderRouter';
 import App from '../App';
@@ -14,9 +14,54 @@ describe('Testa funcionalidade da tela produtos', () => {
 
   afterEach(() => { jest.clearAllMocks(); });
 
-  it('Testa se a página de register está na rota /customer/products', () => {
+  it('Testa se a página de products está na rota /customer/products', () => {
     history.push('/customer/products');
 
     expect(history.location.pathname).toBe('/customer/products');
+  });
+
+  it('Testa se é possível adicionar e remover produtos do carrinho', () => {
+    history.push('/customer/products');
+
+    const quantity = screen
+      .getByTestId('customer_products__input-card-quantity-1');
+    expect(quantity.value).toBe('0');
+
+    const btnAdd = screen
+      .getByTestId('customer_products__button-card-add-item-1');
+    userEvent.click(btnAdd);
+    expect(quantity.value).toBe('2');
+
+    const btnRemove = screen
+      .getByTestId('customer_products__button-card-rm-item-1');
+    userEvent.click(btnRemove);
+    expect(quantity.value).toBe('1');
+  });
+
+  it('Testa se não é possível ter um número negativo de items', () => {
+    history.push('/customer/products');
+
+    const quantity = screen
+      .getByTestId('customer_products__input-card-quantity-1');
+    expect(quantity.value).toBe('0');
+
+    const btnRemove = screen
+      .getByTestId('customer_products__button-card-rm-item-1');
+    userEvent.click(btnRemove);
+    expect(quantity.value).toBe('0');
+
+    userEvent.type(quantity, '-1');
+    expect(quantity.value).toBe('0');
+  });
+
+  it('Testa se é possível fazer logout', () => {
+    history.push('/customer/products');
+
+    const btnlogout = screen
+      .getByTestId('customer_products__element-navbar-link-logout');
+    userEvent.click(btnlogout);
+
+    screen.getByTestId('common_login__input-email');
+    expect(history.location.pathname).toBe('/login');
   });
 });
--- front-end/src/tests/Register.test.js
+++ front-end/src/tests/Register.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import renderWithRouter from './RenderRouter';
 import App from '../App';
