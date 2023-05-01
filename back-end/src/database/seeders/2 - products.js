03c1656129201afe21fb22873349e5d06c478ad7
Welyson Rosa <welysonsr@gmail.com>
2023-05-01 13:02:50 -0300

    feat: add img ao site https://www.flickr.com/

--- back-end/src/database/seeders/2 - products.js
+++ back-end/src/database/seeders/2 - products.js
@@ -8,67 +8,67 @@ module.exports = {
           id: 1,
           name: 'Skol Lata 250ml',
           price: 2.20,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/skol_lata_350ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52862218693_6906e0836a_z.jpg'
       },
       {
           id: 2,
           name: 'Heineken 600ml',
           price: 7.50,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/heineken_600ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861204077_b618e29836_w.jpg'
       },
       {
           id: 3,
           name: 'Antarctica Pilsen 300ml',
           price: 2.49,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/antarctica_pilsen_300ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861783781_2602531eb8_n.jpg'
       },
       {
           id: 4,
           name: 'Brahma 600ml',
           price: 7.50,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/brahma_600ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52862218778_229037d7ff_n.jpg'
       },
       {
           id: 5,
           name: 'Skol 269ml',
           price: 2.19,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/skol_269ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861204097_53a62b316d_n.jpg'
       },
       {
           id: 6,
           name: 'Skol Beats Senses 313ml',
           price: 4.49,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/skol_beats_senses_313ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861783686_67297c32c1_n.jpg'
       },
       {
           id: 7,
           name: 'Becks 330ml',
           price: 4.99,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/becks_330ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861204167_381af5b540_n.jpg'
       },
       {
           id: 8,
           name: 'Brahma Duplo Malte 350ml',
           price: 2.79,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/brahma_duplo_malte_350ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861948969_4b6f5cd471_n.jpg'
       },
       {
           id: 9,
           name: 'Becks 600ml',
           price: 8.89,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/becks_600ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861948999_f0062cced2_n.jpg'
       },
       {
           id: 10,
           name: 'Skol Beats Senses 269ml',
           price: 3.57,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/skol_beats_senses_269ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861204097_53a62b316d_z.jpg'
       },
       {
           id: 11,
           name: 'Stella Artois 275ml',
           price: 3.49,
-          url_image: 'https://deliveryapp-production-ca7e.up.railway.app/images/stella_artois_275ml.jpg'
+          url_image: 'https://live.staticflickr.com/65535/52861948929_cd483c246c_n.jpg'
       }
     ], {});
    
