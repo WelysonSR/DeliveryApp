9820ff7693a40382abdb5cb33066684f157fea7b
Welyson Rosa <welysonsr@gmail.com>
2023-05-03 15:18:02 -0300

    feat: atualizando variavel de ambinete

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 const options = {
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  database:process.env.MYSQL_DB_NAME,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  database:process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
