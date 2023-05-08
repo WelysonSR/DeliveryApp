774091f0cbcfb63dfd1daaccba4765dd65ebd12d
Welyson Rosa <welysonsr@gmail.com>
2023-05-08 19:38:05 -0300

    fix: atualização indicada pelo sonarcloud

--- front-end/src/tests/helpers/api.js
+++ front-end/src/tests/helpers/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-export default api = axios.create({
+const api = axios.create({
   baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3001'}`,
 });
+export default api;
